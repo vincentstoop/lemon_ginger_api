22465333e8bd2353974cfd4ddd8f5df9f808575d
vincentstoop <vincentstoop@gmail.com>
2017-02-27 22:07:30 +0100

    Adjust admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   get 'recipes/index'
 
   namespace :api do
+    resources :recipes, only: [:show, :index]
+    resources :categories, only: [:show, :index]
   end
 
   namespace :admin do
+    resources :recipes
+    resources :categories
   end
 
-  devise_for :admins
+  devise_for :admin
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'pages#home'
