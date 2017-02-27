acbd1159b5f68d2ee7cfd666febfae627b08bb93
vincentstoop <vincentstoop@gmail.com>
2017-02-27 15:06:38 +0100

    Update jointable between categories and recipes

--- db/migrate/20170227140412_add_join_table_categories_recipes.rb
+++ db/migrate/20170227140412_add_join_table_categories_recipes.rb
@@ -1,4 +1,8 @@
 class AddJoinTableCategoriesRecipes < ActiveRecord::Migration[5.1]
   def change
+    create_join_table :categories, :recipes do |t|
+      t.index [:category_id, :recipe_id]
+      t.index [:recipe_id, :category_id]
+    end
   end
 end
