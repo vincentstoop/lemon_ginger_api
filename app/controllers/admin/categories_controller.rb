85bbbab5cfd2514e59a1c39401e5449b8d466873
vincentstoop <vincentstoop@gmail.com>
2017-02-28 14:50:55 +0100

    Update CategoriesController

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,14 +4,37 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def create
+    @category = Category.new(category_params)
 
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category was saved.'
+    else
+      render :index, alert: "Category couldn't be saved."
+    end
   end
 
   def update
+    @category = Category.find(params[:id])
 
+    if @category.update
+      redirect_to admin_categories_path, notice: 'Category was updated.'
+    else
+      render :index, alert: "Category couldn't be updated."
+    end
   end
 
   def destroy
+    category = Category.find(params[:id])
 
+    if category.destroy
+      redirect_to admin_categories_path, notice: 'Category was deleted.'
+    else
+      redirect_to admin_categories_path, alert: "Category couldn't be deleted."
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
