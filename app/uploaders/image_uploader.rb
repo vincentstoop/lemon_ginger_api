cb8950744853d388bef88c37240d3a67b9bad2e5
femkeboermans <femkeboermans@gmail.com>
2017-02-28 12:35:50 +0100

    final checks for photo actions

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,10 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
 
+  # version :thumb do
+  #   process :resize_to_fill => [200,200]
+  # end
+
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -12,7 +16,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -21,10 +28,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
