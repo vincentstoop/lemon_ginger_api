15ddf3fd28f380931a4333d5d98cbf3a91eb83ed
femkeboermans <femkeboermans@gmail.com>
2017-02-28 10:14:32 +0100

    create and destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,5 @@
 class PhotosController < ApplicationController
 
-  def index
-    @photos = Photo.all
-  end
-
-  def show
-    @photo = Photo.find(params[:id])
-  end
-
-  def new
-    @photo = Photo.new
-  end
-
   def create
     @photo = Photo.new
     if @photo.save(photo_params)
@@ -19,27 +7,12 @@ class PhotosController < ApplicationController
     end
   end
 
-  def edit
-    @photo = Photo.find(params[:id])
-  end
-
-  def update
-    @photo = Photo.find(params[:id])
-    if @photo.update(photo_params)
-      redirect_to photo_path, notice: 'Photo replaced'
-    else
-      redirect_to root_path
-    end
-  end
-
   def destroy
     @photo = Photo.find(params[:id])
+    @recipe = @photo.recipe
     @photo.destroy
-    if params[:controller] == "photos"
-      redirect_to information_path, notice: 'Photo deleted'
-    else
-      redirect_to root_path, notice: 'Photo deleted'
-    end
+    @photos = Photo.where(recipe_id: @recipe.id)
+      redirect_to recipe_path
   end
 
   private
