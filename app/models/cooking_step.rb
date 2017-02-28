b8a6de849fa6c6159743acfdd3608fa038010f57
vincentstoop <vincentstoop@gmail.com>
2017-02-28 17:07:09 +0100

    Add CookingStep validations

--- app/models/cooking_step.rb
+++ app/models/cooking_step.rb
@@ -1,3 +1,5 @@
 class CookingStep < ApplicationRecord
   belongs_to :recipe
+  validates :cooking_time, numericality: { only_integer: true }, allow_nil: true
+  validates :description, presence: true
 end
