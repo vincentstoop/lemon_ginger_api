37d453d70a96daaf6e78c89c89f4419d92f3666d
vincentstoop <vincentstoop@gmail.com>
2017-02-28 22:10:43 +0100

    Edit json output RecipesController index&show

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,13 +3,21 @@ class Api::RecipesController < ApplicationController
     recipes = Recipe.all
     render status:200, json: {
       recipes: recipes
-    }.to_json({include: [:cooking_steps, :ingredients, :categories, :photos] })
+    }.to_json({include: [:cooking_steps,
+                          {:ingredients => {only: [:amount, :optional],
+                                            include: {:product => {
+                                              only: [:name, :description]}}}},
+                          :categories, :photos] })
   end
 
   def show
     recipe = Recipe.find(params[:id])
     render status:200, json: {
       recipe: recipe
-    }.to_json({include: [:cooking_steps, :ingredients, :categories, :photos] })
+    }.to_json({include: [:cooking_steps,
+                          {:ingredients => {only: [:amount, :optional],
+                                            include: {:product => {
+                                              only: [:name, :description]}}}},
+                          :categories, :photos] })
   end
 end
