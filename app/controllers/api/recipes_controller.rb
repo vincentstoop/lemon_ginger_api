1c87c191ce5f7575b3197a9549ca53881b89b7ca
vincentstoop <vincentstoop@gmail.com>
2017-03-01 11:10:10 +0100

    Adjust RecipesController and RecipeSerializer

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,24 +1,11 @@
 class Api::RecipesController < ApplicationController
   def index
     recipes = Recipe.all
-    # render status:200, json: {
-    #   recipes: recipes
-    # }.to_json({include: [:cooking_steps,
-    #                       {:ingredients => {only: [:amount, :optional],
-    #                                         include: {:product => {
-    #                                           only: [:name, :description]}}}},
-    #                       :categories, :photos] })
     render status: 200, json: recipes
   end
 
   def show
     recipe = Recipe.find(params[:id])
-    render status:200, json: {
-      recipe: recipe
-    }.to_json({include: [:cooking_steps,
-                          {:ingredients => {only: [:amount, :optional],
-                                            include: {:product => {
-                                              only: [:name, :description]}}}},
-                          :categories, :photos] })
+    render status: 200, json: recipe
   end
 end
--- app/serializers/api/recipe_serializer.rb
+++ app/serializers/api/recipe_serializer.rb
@@ -1,11 +1,9 @@
 class Api::RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :cooking_time, :featured, :published, :intro, :persons,
-  :ingredients, :cooking_steps, :categories
+  attributes :id, :title, :subtitle, :cooking_time, :featured, :published, :intro
 
   has_many :categories
   has_many :ingredients
   has_many :cooking_steps
-  # has_many :products, through: :ingredients
   has_many :users
   has_many :photos
 end
