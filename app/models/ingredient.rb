449a10fcaab12787b5ab906887776ffc6ec71e92
vincentstoop <vincentstoop@gmail.com>
2017-03-01 18:39:47 +0100

    Add validations to Ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,6 @@ class Ingredient < ApplicationRecord
   belongs_to :product
 
   validates :optional, inclusion: {in: [true, false]}
+  validates :product, presence: true
+  validates :recipe, presence: true
 end
