64f99cfe9ea095dac0acb7e5f62b4ad0cc827427
vincentstoop <vincentstoop@gmail.com>
2017-03-01 10:23:30 +0100

    Update RecipeSerializer

--- app/serializers/api/recipe_serializer.rb
+++ app/serializers/api/recipe_serializer.rb
@@ -1,10 +1,11 @@
 class Api::RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :cooking_time, :intro, :featured, :published, :persons
+  attributes :id, :title, :subtitle, :cooking_time, :featured, :published, :intro, :persons,
+  :ingredients, :cooking_steps, :categories
 
   has_many :categories
   has_many :ingredients
-  has_many :products, through: :ingredients
-  has_many :users
   has_many :cooking_steps
+  # has_many :products, through: :ingredients
+  has_many :users
   has_many :photos
 end
