1115fdf2037a73d3d3f118477daadb99bfa6562d
Wouter de Vos <wouter@codaisseur.com>
2017-03-03 14:44:24 +0100

    Switch to S3 for production storage

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3.6'
+gem 'fog-aws'
 
 # gem 'redis', '~> 3.0'
 # gem 'bcrypt', '~> 3.1.7'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ include CarrierWave::MiniMagick
   end
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage Rails.env.production? ? :fog : :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -35,9 +35,9 @@ include CarrierWave::MiniMagick
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
